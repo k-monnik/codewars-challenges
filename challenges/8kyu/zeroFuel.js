0500089737e8d7e6d396a57c0fdd0c2445091176
Kristen Monnik <nolaviolinist@gmail.com>
2022-05-30 20:45:49 -0400

    add zeroFuel solution

--- challenges/8kyu/zeroFuel.js
+++ challenges/8kyu/zeroFuel.js
@@ -1,3 +1,9 @@
 const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
-    // TODO
+    if (distanceToPump / mpg > fuelLeft) {
+        return false;
+        console.log("You will have to walk to the gas station.")
+    } else {
+        return true;
+        console.log("You will make it home!")
+    }
 };
