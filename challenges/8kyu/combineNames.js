1cfad7264b5e4c3a4a090a283c41e4322f0e1981
Kristen Monnik <nolaviolinist@gmail.com>
2022-07-22 23:39:33 -0400

    add combineNames challenge and solution

---
+++ challenges/8kyu/combineNames.js
@@ -0,0 +1,13 @@
+// Combine strings function
+// Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+// Example:
+
+// combineNames('James', 'Stevens')
+// returns:
+
+// 'James Stevens'
+
+function combineNames(name1, name2) {
+    return (name1 + " " + name2);
+}
